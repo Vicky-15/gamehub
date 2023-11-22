793607a9e34d2b351d5d5556173891d4966b7ac4
Vicky-15 <vigneshstudent98@gmail.com>
2023-11-22 16:24:30 +0530

    displayed genres in sidebar

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,13 +1,26 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenresList = () => {
   const { data } = useGenres();
 
   return (
     <>
-      {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
-      ))}
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="10px">
+            <HStack>
+              <Image
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+                boxSize="32px"
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 };
