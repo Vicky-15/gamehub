b83366d226a80ed54494e16f2feeb8b4c5b5cb51
Vicky-15 <vigneshstudent98@gmail.com>
2023-12-06 14:37:05 +0530

    remove: react query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme.ts";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
